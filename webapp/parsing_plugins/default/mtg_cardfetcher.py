c90f3dc1236541c4b3921dfc67800a36b7c5bca8
Christopher Chen <christoipher.chen@gmail.com>
2018-05-16 12:19:27 -0700

    Change MTG card recognizer to not make API call for every card, instead yield a search link.

--- webapp/parsing_plugins/default/mtg_cardfetcher.py
+++ webapp/parsing_plugins/default/mtg_cardfetcher.py
@@ -6,8 +6,10 @@ from webapp import models
 import re
 import scrython
 import aiohttp
+
 card_pattern = re.compile(r'\[\[[^\[^\].]*\]\]')
 link_element_template = '<a target="_blank" href="{link}">{body}</a>'
+base_url = 'https://scryfall.com/search?q='
 
 
 class Plugin(parsing.Plugin):
@@ -21,9 +23,5 @@ class Plugin(parsing.Plugin):
         for card in cards:
             if card not in seen:
                 seen.add(card)
-                try:
-                    card_obj = scrython.cards.Named(fuzzy=card[2:-2])
-                    yield link_element_template.format(
-                        link=card_obj.scryfall_uri(), body=card_obj.name())
-                except aiohttp.client_exceptions.ClientConnectorError:
-                    pass
+                cardname = card[2:-2]
+                yield link_element_template.format(link=base_url + '+'.join(cardname.split(' ')), body=cardname)
