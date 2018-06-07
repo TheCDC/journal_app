af3c698cd8bc9d008add9e59958c8fc1abcb4313
Christopher Chen <christopher.chen1995@gmail.com>
2018-06-06 21:04:26 -0700

    Try regex method

--- webapp/parsing_plugins/default/mtg_cardfetcher.py
+++ webapp/parsing_plugins/default/mtg_cardfetcher.py
@@ -13,124 +13,8 @@ import os
 import datetime
 import dateutil.parser
 import logging
-card_pattern = re.compile(r'\[\[[^\[^\].]*\]\]')
-word_pattern = re.compile(r'\b[^\W_]+\b')
 link_element_template = '<a target="_blank" href="{link}">{body}</a>'
 base_url = 'https://scryfall.com/search?q=!'
-
-
-class Node:
-
-    def __init__(self, value):
-        self.value = value
-        self.children = list()
-        self.parent = None
-
-    def add_child(self, node):
-        node.parent = self
-        self.children.append(node)
-        # self.children.sort(key=lambda n: n.value)
-
-    def get_phrase(self):
-        n = self
-        ancestry = []
-        while n.parent is not None:
-            ancestry.insert(0, n.value)
-            n = n.parent
-        return ancestry
-
-    def __str__(self):
-        ancestry = self.get_phrase()
-        return f'Node < "{self.value}", {len(self.children)} children >'
-    def __hash__(self):
-        return hash((self.value,))
-
-
-class SuffixTree:
-
-    def __init__(self, list_of_phrases, comparator=None):
-        self.root = Node(None)
-        self.memory = dict()
-        if comparator:
-            self.compare_function = comparator
-        else:
-            self.compare_function = lambda a, b: a == b
-        for p in list_of_phrases:
-            self.search(p, _insert=True)
-
-    def search(self, list_of_words, _insert=False):
-        """Return a tuple of (node, remaining_tokens).
-        The search consumes tokens from list_of_words as it performs the search.
-        Un-consumed tokens are return in remaining_tokens.
-        """
-        words_iter = iter(list_of_words)
-        words_tuple = tuple(list_of_words)
-        cur_token = next(words_iter)
-        cur_node = self.root
-        if words_tuple in self.memory:
-            return self.memory[words_tuple]
-        while True:
-            # guard
-            if len(cur_node.children) == 0 and not _insert:
-                # memoize search and return value
-                ret = (cur_node, list(words_iter))
-                if words_tuple not in self.memory:
-                    self.memory.update({words_tuple: ret})
-                return self.memory[words_tuple]
-            # check children for match
-            for n in cur_node.children:
-                if self.compare_function(n.value, cur_token):
-                    cur_node = n
-                    try:
-                        cur_token = next(words_iter)
-                    except StopIteration:
-                        # memoize search and return value
-                        ret = (cur_node, list(words_iter))
-                        if words_tuple not in self.memory:
-                            self.memory.update({words_tuple:ret})
-                        return self.memory[words_tuple]
-                    break
-            else:
-                # no matching children
-                if not _insert:
-                    return (cur_node, [cur_token] + list(words_iter))
-                # make new node
-                new_node = Node(cur_token)
-                cur_node.add_child(new_node)
-                cur_node = new_node
-                try:
-                    cur_token = next(words_iter)
-                except StopIteration:
-                    # memoize search and return value
-                    ret = (cur_node, list(words_iter))
-                    if words_tuple not in self.memory:
-                        self.memory.update({words_tuple: ret})
-                    return self.memory[words_tuple]
-    def __str__(self):
-        outlines = []
-        d = 0
-        stack = []
-        stack.append((self.root, 0))
-        while len(stack) > 0:
-            cur = stack.pop()
-            d = cur[1]
-            outlines.append('  ' * d + str(cur[0]))
-            for c in cur[0].children:
-                stack.append((c, d + 1))
-        return '\n'.join(outlines)
-
-    def find_all(self, list_of_words):
-        search_result = self.search(list_of_words)
-        found_node = search_result[0]
-        if found_node is self.root or len(search_result[1]) > 0:
-            return
-        stack = [found_node]
-        while len(stack) > 0:
-            cur = stack.pop()
-            if len(cur.children) == 0:
-                yield cur
-            else:
-                stack.extend(cur.children)
 
 
 url = 'http://mtgjson.com/json/AllCards.json.zip'
@@ -165,12 +49,25 @@ def fetch(mailbox, target):
             meta_info_file.write(json.dumps(obj))
     td = datetime.timedelta(days=1)
     if (now  - updated_at) > td or not os.path.exists(target):
+
+        print('download cards db')
         logging.info('MTG database too old. Updating...')
         download_cards_to_file(target)
+        with open(meta_path,'w') as meta_info_file:
+            updated_at = now
+            obj = {
+                'updated_at':updated_at.isoformat()
+            }
+            meta_info_file.write(json.dumps(obj))
     with open(target) as f:
         cards = json.load(f)
-    cards_tree = SuffixTree([list(tokenize(name)) for name in cards], comparator=lambda a, b: a.lower() == b.lower())
-    mailbox.append(cards_tree)
+    print('start build re pattern')
+    pattern = '(' + '|'.join(re.escape(name )for name in cards) + ')'
+    print(pattern)
+    cards_patttern = re.compile(pattern)
+    print('built re pattern')
+    mailbox.append(cards_patttern)
+    return
 
 
 def identify_cards(s, cards_tree):
@@ -204,16 +101,21 @@ class Plugin(parsing.Plugin):
         self.cards_file_path = os.path.join(self.resources_path, 'cards.json')
         self.thread = threading.Thread(target=fetch, kwargs=dict(mailbox=self.queue, target=self.cards_file_path))
         self.thread.start()
-        self.cards_tree = None
+        self.cards_pattern = None
 
     def parse_entry(self, e: models.JournalEntry) -> 'iterable[str]':
-        if self.cards_tree is None:
+        if self.cards_pattern is None:
             if self.thread.isAlive():
                 yield "Card database being built."
                 return
             else:
-                self.cards_tree = self.queue.pop()
-
-        for c in identify_cards(e.contents, self.cards_tree):
-            cardname = ''.join(c.get_phrase())
-            yield link_element_template.format(link=base_url + '&quot '+ '+'.join(cardname.split(' ')) + '&quot', body=cardname)
+                self.cards_pattern = self.queue.pop()
+        found = self.cards_pattern.finditer(e.contents)
+        print('found',found)
+        seen = set()
+        for cardname in found:
+            cardname = cardname.group(1)
+            if cardname not in found:
+                seen.add(cardname)
+                print(cardname)
+                yield link_element_template.format(link=base_url + '&quot '+ '+'.join(cardname.split(' ')) + '&quot', body=cardname)
