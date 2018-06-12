cccda006315684d1f355cd973c0319668344b8f4
Christopher Chen <christopher.chen1995@gmail.com>
2018-06-12 12:58:35 -0700

    Hopefully fix out of date heroku db

--- webapp/journal_plugins/name_search/models.py
+++ webapp/journal_plugins/name_search/models.py
@@ -1,7 +1,10 @@
+
 from webapp import extensions
 from webapp import models
 
 import datetime
+from sqlalchemy.sql import func
+
 from sqlalchemy.orm import backref
 from webapp.journal_plugins import name_search
 
@@ -12,7 +15,7 @@ class NameSearchCache(db.Model):
     parent_id = db.Column(db.Integer, db.ForeignKey(models.JournalEntry.id, ondelete="cascade"), primary_key=True,)
     parent = db.relationship(models.JournalEntry, foreign_keys='NameSearchCache.parent_id')
     created_at = db.Column(
-        db.Date, default=datetime.datetime.utcnow)
+        db.DateTime, default=func.now(),nullable=False)
     updated_at = db.Column(
-        db.Date, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+        db.DateTime, default=func.now(), onupdate=func.now(),nullable=False)
     json = db.Column(db.String)
