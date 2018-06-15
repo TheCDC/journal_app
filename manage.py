8a4bac6709ba42af26074d3d88565316c93d6c0a
Christopher Chen <christopher.chen1995@gmail.com>
2018-06-15 01:34:36 -0700

    Allow output of length 0 (zero) from plugins.

--- manage.py
+++ manage.py
@@ -13,7 +13,8 @@ def re_process_all_plugins():
     logging.warning('Long running operation hhas ben initiated! All entries are being processed by plugins.')
     for e in models.JournalEntry.query.all():
         for item in (plugin_manager.parse_entry(e)):
-            list(item['output'])
+            if item['output'] is not None:
+                list(item['output'])
     logging.warning('All entries have been processed by plugins.')
 
 
