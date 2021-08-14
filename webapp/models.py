dfa95aedd8e3c53558f73493adfda1b66e1eb3cd
Christopher Chen <christopher.chen1995@gmail.com>
2021-08-13 18:48:40 -0700

    Make app run in 2021

--- webapp/models.py
+++ webapp/models.py
@@ -16,11 +16,11 @@ from sqlalchemy.orm import backref
 logger = logging.getLogger(__name__)
 
 # Define models
-roles_users = db.Table('roles_users',
-                       db.Column('user_id', db.Integer(),
-                                 db.ForeignKey('user.id')),
-                       db.Column('role_id', db.Integer(),
-                                 db.ForeignKey('role.id')))
+roles_users = db.Table(
+    "roles_users",
+    db.Column("user_id", db.Integer(), db.ForeignKey("user.id")),
+    db.Column("role_id", db.Integer(), db.ForeignKey("role.id")),
+)
 
 
 class Role(db.Model, RoleMixin):
@@ -40,9 +40,8 @@ class User(db.Model, UserMixin):
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship(
-        'Role',
-        secondary=roles_users,
-        backref=db.backref('users', lazy='dynamic'))
+        "Role", secondary=roles_users, backref=db.backref("users", lazy="dynamic")
+    )
 
     # ========== flask-login methods ==========
     @property
@@ -60,33 +59,44 @@ class User(db.Model, UserMixin):
     def get_id(self):
         return self.id
 
-    def get_latest_entry(self, ) -> "JournalEntry":
+    def get_latest_entry(
+        self,
+    ) -> "JournalEntry":
         """Return the chronologically latest JournalEntry."""
         # session = db.session()
         # session.add(self)
-        return JournalEntry.query.filter(JournalEntry.owner_id == self.id).order_by(
-            JournalEntry.create_date.desc()).first()
+        return (
+            JournalEntry.query.filter(JournalEntry.owner_id == self.id)
+            .order_by(JournalEntry.create_date.desc())
+            .first()
+        )
 
     def query_all_entries(self):
         return JournalEntry.query.filter(JournalEntry.owner_id == self.id)
 
-    def get_all_years(self, ) -> 'iterable[datetime.datetime]':
+    def get_all_years(
+        self,
+    ) -> "iterable[datetime.datetime]":
         """Return a list of dates corresponding to the range of
         years encompassed by all journal entries."""
         # define earliest and latest years of entries
-        start_year = self.query_all_entries().order_by(
-            JournalEntry.create_date).first()
-        end_year = self.query_all_entries().order_by(
-            JournalEntry.create_date.desc()).first()
+        start_year = self.query_all_entries().order_by(JournalEntry.create_date).first()
+        end_year = (
+            self.query_all_entries().order_by(JournalEntry.create_date.desc()).first()
+        )
         if start_year and end_year:
-            for y in range(start_year.create_date.year,
-                           end_year.create_date.year + 1):
+            for y in range(start_year.create_date.year, end_year.create_date.year + 1):
                 # find any entry within this year but before next year
-                found = self.query_all_entries().filter(
-                    JournalEntry.create_date >= datetime.datetime(
-                        y, 1, 1, 0, 0)).filter(
-                    JournalEntry.create_date < datetime.datetime(
-                        y + 1, 1, 1, 0, 0)).first()
+                found = (
+                    self.query_all_entries()
+                    .filter(
+                        JournalEntry.create_date >= datetime.datetime(y, 1, 1, 0, 0)
+                    )
+                    .filter(
+                        JournalEntry.create_date < datetime.datetime(y + 1, 1, 1, 0, 0)
+                    )
+                    .first()
+                )
                 # only yield this year if has an entry
                 if found:
                     yield datetime.datetime(y, 1, 1, 0, 0)
@@ -94,32 +104,41 @@ class User(db.Model, UserMixin):
     def next_entry(self, e: "JournalEntry") -> "JournalEntry":
         """Return the first JournalEntry that falls chronologically after
         the given entry."""
-        return self.query_all_entries().filter(
-            JournalEntry.create_date > e.create_date).first()
+        return (
+            self.query_all_entries()
+            .filter(JournalEntry.create_date > e.create_date)
+            .first()
+        )
 
     def previous_entry(self, e: "JournalEntry") -> "JournalEntry":
         """Return the first JournalEntry that falls chronologically before
         the given entry."""
-        return self.query_all_entries().filter(
-            JournalEntry.create_date < e.create_date).order_by(
-            JournalEntry.create_date.desc()).first()
+        return (
+            self.query_all_entries()
+            .filter(JournalEntry.create_date < e.create_date)
+            .order_by(JournalEntry.create_date.desc())
+            .first()
+        )
 
 
 class JournalEntry(db.Model):
     """Model for journal entries."""
+
     id = db.Column(db.Integer, primary_key=True)
-    create_date = db.Column(
-        db.Date, default=datetime.datetime.utcnow, index=True)
+    create_date = db.Column(db.Date, default=datetime.datetime.utcnow, index=True)
     contents = db.Column(db.String)
     owner_id = db.Column(db.Integer, db.ForeignKey(User.id))
-    owner = db.relationship(User, backref=backref('entries', cascade="all,delete"), )
-    __table__args = (db.UniqueConstraint('id', 'create_date', name='_id_date'),)
+    owner = db.relationship(
+        User,
+        backref=backref("entries", cascade="all,delete"),
+    )
+    __table__args = (db.UniqueConstraint("id", "create_date", name="_id_date"),)
 
     def __str__(self):
         return repr(self.id)
 
     def __repr__(self):
-        return f'< JournaEntry id={self.id} create_date={self.create_date}'
+        return f"< JournaEntry id={self.id} create_date={self.create_date}"
 
     @property
     def html(self) -> str:
@@ -129,17 +148,20 @@ class JournalEntry(db.Model):
     @property
     def date_string(self) -> str:
         """Date as YYYY-MM-DD."""
-        return self.create_date.strftime('%Y-%m-%d')
+        return self.create_date.strftime("%Y-%m-%d")
 
     @property
     def date_human(self) -> str:
         """A pretty and human readable date."""
-        return self.create_date.strftime('%B %d, %Y, a %A')
+        return self.create_date.strftime("%B %d, %Y, a %A")
 
     @property
     def next(self):
-        found = JournalEntry.query.filter(JournalEntry.owner_id == self.owner.id).filter(
-            JournalEntry.create_date > self.create_date).first()
+        found = (
+            JournalEntry.query.filter(JournalEntry.owner_id == self.owner.id)
+            .filter(JournalEntry.create_date > self.create_date)
+            .first()
+        )
         if found:
             if found.id != self.id:
                 return found
@@ -148,9 +170,12 @@ class JournalEntry(db.Model):
     @property
     def previous(self):
 
-        found = self.owner.query_all_entries().filter(
-            JournalEntry.create_date < self.create_date).order_by(
-            JournalEntry.create_date.desc()).first()
+        found = (
+            self.owner.query_all_entries()
+            .filter(JournalEntry.create_date < self.create_date)
+            .order_by(JournalEntry.create_date.desc())
+            .first()
+        )
 
         if found:
             if found.id != self.id:
@@ -168,17 +193,17 @@ class PluginConfig(db.Model):
 
 class JournalEntryView(ModelView):
     column_list = (
-        'owner',
-        'create_date',
-        'contents',
+        "owner",
+        "create_date",
+        "contents",
     )
 
     def summary(view, context, model, name):
         s = model.contents
-        return s[:30] + (len(s) > 30) * '...'
+        return s[:30] + (len(s) > 30) * "..."
 
     column_formatters = {
-        'contents': summary,
+        "contents": summary,
     }
 
 
@@ -188,12 +213,12 @@ security = Security(app, user_datastore)
 
 if config.DEBUG_ENABLED:
     admin.add_view(
-        JournalEntryView(
-            JournalEntry, db.session, endpoint='model_view_journalentry'))
+        JournalEntryView(JournalEntry, db.session, endpoint="model_view_journalentry")
+    )
     admin.add_view(
-        ModelView(
-            PluginConfig, db.session, endpoint='model_view_pluginconfig'))
-    admin.add_view(ModelView(User, db.session, endpoint='model_view_user'))
+        ModelView(PluginConfig, db.session, endpoint="model_view_pluginconfig")
+    )
+    admin.add_view(ModelView(User, db.session, endpoint="model_view_user"))
 
 
 def instantiate_db(app):
@@ -203,34 +228,47 @@ def instantiate_db(app):
         try:
             flask_migrate.init(webapp.config.ALEMBIC_PATH)
         except alembic.util.exc.CommandError as e:
-            if 'already exists' in str(e):
+            if "already exists" in str(e):
                 pass
             else:
-                logger.debug('flask db init failed: %s', e)
+                logger.debug("flask db init failed: %s", e)
                 raise e
         flask_migrate.migrate(webapp.config.ALEMBIC_PATH)
         try:
-            logger.debug('flask db upgrade')
+            logger.debug("flask db upgrade")
             flask_migrate.upgrade(webapp.config.ALEMBIC_PATH)
         except Exception as e:
-            logger.debug('flask db upgrade failed: %s', e)
+            logger.debug("flask db upgrade failed: %s", e)
             raise e
 
 
 # ========== Marshmallow Schemas ==========
 
-class UserSchema(marshmallow.ModelSchema):
+
+class UserSchema(marshmallow.SQLAlchemyAutoSchema):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email',)
+        fields = (
+            "id",
+            "username",
+            "email",
+        )
 
 
-class JournalEntrySchema(marshmallow.ModelSchema):
+class JournalEntrySchema(marshmallow.SQLAlchemyAutoSchema):
     class Meta:
         model = JournalEntry
-        fields = ('id', 'contents', 'create_date', 'url', 'date_human', 'date_string', 'html')
+        fields = (
+            "id",
+            "contents",
+            "create_date",
+            "url",
+            "date_human",
+            "date_string",
+            "html",
+        )
 
-    url = marshmallow.Method('get_url')
+    url = marshmallow.Method("get_url")
 
     def get_url(self, entry):
         return link_for_entry(entry)
---
+++ webapp/parsing_plugins/settings.py
@@ -0,0 +1,2 @@
+"""Edit this file to point the application at installed plugins."""
+installed_plugins = []
