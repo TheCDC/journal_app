b635830dab5e0666dc7abe23d974d5c9518c2338
Christopher Chen <christopher.chen1995@gmail.com>
2021-08-14 10:17:03 -0700

    It's alive!

--- webapp/webapp/webapp/views.py
+++ webapp/webapp/webapp/views.py
@@ -17,7 +17,7 @@ from flask_security.utils import hash_password
 logger = logging.getLogger(__name__)
 
 if logger.disabled:
-    raise RuntimeError('Logger is mistakenly disabled at top level')
+    raise RuntimeError("Logger is mistakenly disabled at top level")
 
 
 class EnableLoggingMixin:
@@ -25,7 +25,7 @@ class EnableLoggingMixin:
         classname = type(self).__name__
         super().__init__(*args, **kwargs)
         # self.logger = logging.getLogger(__name__ + '.IndexView')
-        self._logger = logging.getLogger(__name__ + '.' + classname)
+        self._logger = logging.getLogger(__name__ + "." + classname)
 
     @property
     def logger(self):
@@ -37,15 +37,19 @@ class EnableLoggingMixin:
 class LogoutView(MethodView):
     def get(self):
         flask_login.logout_user()
-        return flask.redirect(flask.url_for('index'))
+        return flask.redirect(flask.url_for("index"))
 
 
 class RegisterView(MethodView):
-    def get_template_name(self, ):
-        return 'register.html'
+    def get_template_name(
+        self,
+    ):
+        return "register.html"
 
     def get(self, **kwargs):
-        context = dict(register_form=forms.RegisterForm(), )
+        context = dict(
+            register_form=forms.RegisterForm(),
+        )
         context.update(kwargs)
         return flask.render_template(self.get_template_name(), context=context)
 
@@ -63,14 +67,16 @@ class RegisterView(MethodView):
             try:
                 db.session.commit()
                 flask_login.login_user(
-                    models.user_datastore.get_user(register_form.email.data))
+                    models.user_datastore.get_user(register_form.email.data)
+                )
             except sqlalchemy.exc.IntegrityError:
                 db.session.rollback()
                 return self.get(
-                    register_form=register_form, error="User already exists!")
+                    register_form=register_form, error="User already exists!"
+                )
 
             # flask.flash('User successfully registered')
-            return flask.redirect(flask.url_for('home'))
+            return flask.redirect(flask.url_for("home"))
         else:
             return self.get(register_form=register_form)
 
@@ -78,17 +84,16 @@ class RegisterView(MethodView):
 class EntrySearchView(MethodView):
     def get_objects(self, **kwargs):
         start_date = self.args_to_date(**kwargs)
-        session = kwargs.get('session', db.session())
+        session = kwargs.get("session", db.session())
 
-        found = list(flask_login.current_user.query_all_entries().filter(
-            models.JournalEntry.create_date >= start_date).order_by(
-            models.JournalEntry.create_date))
+        found = list(
+            flask_login.current_user.query_all_entries()
+            .filter(models.JournalEntry.create_date >= start_date)
+            .order_by(models.JournalEntry.create_date)
+        )
         return found
 
-    def args_to_date(self,
-                     year: int = None,
-                     month: int = None,
-                     day: int = None):
+    def args_to_date(self, year: int = None, month: int = None, day: int = None):
 
         start_date = datetime.date(1, 1, 1)
         if year is not None:
@@ -102,10 +107,7 @@ class EntrySearchView(MethodView):
     @flask_login.login_required
     def get(self, **kwargs):
         # return a rendered entry if the date is fully specified
-        my_kwargs = {
-            k: kwargs[k]
-            for k in ['year', 'month', 'day'] if k in kwargs
-        }
+        my_kwargs = {k: kwargs[k] for k in ["year", "month", "day"] if k in kwargs}
         try:
             found = self.get_objects(**my_kwargs)
         except ValueError:
@@ -117,17 +119,20 @@ class EntrySearchView(MethodView):
         e = found[0]
         plugins_output = list(plugin_manager.parse_entry(e))
         session = db.session()
-        e = session.query(models.JournalEntry).filter(models.JournalEntry.id == e.id).first()
+        e = (
+            session.query(models.JournalEntry)
+            .filter(models.JournalEntry.id == e.id)
+            .first()
+        )
         for o in [e.next, e.previous]:
             if o:
                 session.add(o)
         # plugins_output = [(obj['plugin']['name'], list(obj['output'])) for obj in plugin_manager.parse_entry(e)]
         context = dict(
-            entry=models.journal_entry_schema.dump(obj=e).data,
-            next=models.journal_entry_schema.dump(obj=e.next).data,
-            previous=models.journal_entry_schema.dump(obj=e.previous).data,
+            entry=models.journal_entry_schema.dump(obj=e),
+            next=models.journal_entry_schema.dump(obj=e.next),
+            previous=models.journal_entry_schema.dump(obj=e.previous),
             plugins_output=plugins_output,
-
         )
         # handle incompletely specified date
         # take the user to a search
@@ -139,75 +144,80 @@ class EntrySearchView(MethodView):
             flask.abort(404)
         forward = flask_login.current_user.next_entry(e)
         backward = flask_login.current_user.previous_entry(e)
-        return flask.render_template(
-            'entry.html',
-            context=context)
+        return flask.render_template("entry.html", context=context)
 
 
 class IndexView(MethodView):
     def get_template_name(self):
-        return 'index.html'
+        return "index.html"
 
     def get(self):
         if flask_login.current_user.is_authenticated:
-            return flask.redirect(flask.url_for('home'))
+            return flask.redirect(flask.url_for("home"))
         return flask.render_template(self.get_template_name())
 
 
 class HomeView(MethodView, EnableLoggingMixin):
     def get_template_name(self):
-        return 'home.html'
+        return "home.html"
 
     @flask_login.login_required
     def post(self, **kwargs):
         upload_form = forms.UploadForm()
-        self.logger.debug('Try upload')
+        self.logger.debug("Try upload")
         good_parse = False
         if upload_form.validate_on_submit():
-            logger.debug('Invalid form')
+            logger.debug("Invalid form")
             file = flask.request.files[upload_form.file.name]
             session = db.session()
             try:
-                parsed = parsing.identify_entries(
-                    file.read().decode().split('\n'))
+                parsed = parsing.identify_entries(file.read().decode().split("\n"))
             except ValueError as e:
                 # upload_form.file.errors
-                self.logger.debug('parse failed')
+                self.logger.debug("parse failed")
                 return self.get(upload_form=upload_form, error=e)
 
             flask_login.current_user.query_all_entries().delete()
             for e in parsed:
-                body_text = e.body.replace('\r', '')
-                found = flask_login.current_user.query_all_entries().filter_by(
-                    create_date=e.date).first()
+                body_text = e.body.replace("\r", "")
+                found = (
+                    flask_login.current_user.query_all_entries()
+                    .filter_by(create_date=e.date)
+                    .first()
+                )
                 if found:
                     found.contents = body_text
                 else:
                     found = models.JournalEntry(
                         owner=flask_login.current_user,
                         create_date=e.date,
-                        contents=body_text)
+                        contents=body_text,
+                    )
                 session.add(found)
 
             session.flush()
             session.commit()
-            self.logger.debug('parse succeeded')
+            self.logger.debug("parse succeeded")
             good_parse = True
         if good_parse:
             return self.get(
-                upload_form=upload_form,
-                success='Your journal was successfully parsed!')
+                upload_form=upload_form, success="Your journal was successfully parsed!"
+            )
         else:
-            return self.get(
-                upload_form=upload_form, error='Invalid submission!')
+            return self.get(upload_form=upload_form, error="Invalid submission!")
         # return flask.redirect(flask.url_for('index'))
 
     @flask_login.login_required
     def get(self, **kwargs):
         upload_form = forms.UploadForm()
 
-        latest_entry = models.JournalEntry.query.filter(models.JournalEntry.owner == flask_login.current_user).order_by(
-            models.JournalEntry.create_date.desc()).first()
+        latest_entry = (
+            models.JournalEntry.query.filter(
+                models.JournalEntry.owner == flask_login.current_user
+            )
+            .order_by(models.JournalEntry.create_date.desc())
+            .first()
+        )
         # form the context with default values
         context = dict(
             upload_form=upload_form,
@@ -216,7 +226,10 @@ class HomeView(MethodView, EnableLoggingMixin):
             latest_entry=latest_entry,
             now=datetime.datetime.now(),
             days_since_latest=(
-                    datetime.datetime.now().date() - latest_entry.create_date).days if latest_entry else None,
+                datetime.datetime.now().date() - latest_entry.create_date
+            ).days
+            if latest_entry
+            else None,
             error=None,
             success=None,
         )
@@ -230,15 +243,18 @@ class HomeView(MethodView, EnableLoggingMixin):
 
 class SettingsView(MethodView):
     def get_template_name(self):
-        return 'settings.html'
+        return "settings.html"
 
     @flask_login.login_required
     def get(self, **kwargs):
 
         # form = flask_login.current_user.get_settings_form()
-        form = kwargs.get('form', None)
+        form = kwargs.get("form", None)
         if not form:
-            form = forms.UserEditForm(**api.object_as_dict(flask_login.current_user), obj=flask_login.current_user)
+            form = forms.UserEditForm(
+                **api.object_as_dict(flask_login.current_user),
+                obj=flask_login.current_user,
+            )
         context = dict(settings_form=form)
 
         context.update(kwargs)
@@ -258,22 +274,29 @@ class SettingsView(MethodView):
             # cu.update_settings(form)
         else:
             return flask.render_template(context=dict(settings_form=form))
-        return flask.redirect(flask.url_for('settings'))
+        return flask.redirect(flask.url_for("settings"))
 
 
 class EntryEditView(MethodView):
     @flask_login.login_required
     def get(self, **kwargs):
-        found = models.JournalEntry.query.filter_by(id=kwargs['id'], owner=flask_login.current_user).first()
+        found = models.JournalEntry.query.filter_by(
+            id=kwargs["id"], owner=flask_login.current_user
+        ).first()
         if not found:
             flask.abort(404)
         form = forms.JournalEntryEditForm(**api.object_as_dict(found))
-        context = dict(create=False, heading='Edit Entry')
-        context['heading'] = 'Edit entry'
-        context.update(dict(back=api.link_for_entry(found), action=flask.url_for('edit_entry', id=form.id.data),
-                            form=form))
+        context = dict(create=False, heading="Edit Entry")
+        context["heading"] = "Edit entry"
+        context.update(
+            dict(
+                back=api.link_for_entry(found),
+                action=flask.url_for("edit_entry", id=form.id.data),
+                form=form,
+            )
+        )
         context.update(kwargs)
-        return flask.render_template('edit_entry.html', context=context)
+        return flask.render_template("edit_entry.html", context=context)
 
     @flask_login.login_required
     def post(self, **kwargs):
@@ -282,7 +305,9 @@ class EntryEditView(MethodView):
         context = dict(errors=list())
         if form.validate_on_submit():
             if form.owner_id.data == cu.id:
-                obj = models.JournalEntry.query.filter_by(id=form.id.data, owner=cu).first()
+                obj = models.JournalEntry.query.filter_by(
+                    id=form.id.data, owner=cu
+                ).first()
                 form.populate_obj(obj)
                 session = db.session()
                 session.add(obj)
@@ -292,7 +317,7 @@ class EntryEditView(MethodView):
             else:
                 flask.abort(403)
         else:
-            context['errors'].append('form invalid')
+            context["errors"].append("form invalid")
             return self.get(**context)
 
 
@@ -300,14 +325,16 @@ class EntryCreateView(MethodView):
     @flask_login.login_required
     def get(self, **kwargs):
         context = dict(
-            create=True, back=flask.url_for('home'), action=flask.url_for('create_entry'),
-            heading='Create Entry'
+            create=True,
+            back=flask.url_for("home"),
+            action=flask.url_for("create_entry"),
+            heading="Create Entry",
         )
         form = forms.JournalEntryCreateForm()
-        form.contents.data = ''
+        form.contents.data = ""
         context.update(dict(form=form))
         context.update(**kwargs)
-        return flask.render_template('edit_entry.html', context=context)
+        return flask.render_template("edit_entry.html", context=context)
 
     @flask_login.login_required
     def post(self, **kwargs):
@@ -317,17 +344,19 @@ class EntryCreateView(MethodView):
         if form.validate_on_submit():
             obj = models.JournalEntry(owner=flask_login.current_user)
             form.populate_obj(obj)
-            print('obj', obj.owner)
+            print("obj", obj.owner)
             session = db.session()
             try:
                 session.add(obj)
                 session.commit()
             except sqlalchemy.exc.IntegrityError:
                 session.rollback()
-                context['errors'].append('That date already has an entry!')
+                context["errors"].append("That date already has an entry!")
                 return self.get(**context)
 
-            return flask.redirect(api.link_for_entry(flask_login.current_user.get_latest_entry()))
+            return flask.redirect(
+                api.link_for_entry(flask_login.current_user.get_latest_entry())
+            )
 
         else:
             return self.get(**context)
@@ -337,20 +366,22 @@ class ExportJournalView(MethodView):
     @flask_login.login_required
     def get(self, **kwargs):
         cu = flask_login.current_user
-        entries = models.JournalEntry.query.filter_by(owner=cu).order_by(models.JournalEntry.create_date)
+        entries = models.JournalEntry.query.filter_by(owner=cu).order_by(
+            models.JournalEntry.create_date
+        )
 
         def generate():
             for e in entries:
                 yield e.date_string
-                yield '\n'
+                yield "\n"
                 yield e.contents
-                yield '\n'
+                yield "\n"
 
         response = flask.Response(flask.stream_with_context(generate()))
-        filename = f'{cu.first_name} {cu.last_name} journal exported {datetime.datetime.now().isoformat()}.txt'
+        filename = f"{cu.first_name} {cu.last_name} journal exported {datetime.datetime.now().isoformat()}.txt"
         cd = f'attachment; filename="{filename}"'
-        response.headers['Content-Disposition'] = cd
-        response.mimetype = 'application/octet-stream'
+        response.headers["Content-Disposition"] = cd
+        response.mimetype = "application/octet-stream"
         return response
 
 
@@ -358,23 +389,29 @@ class DeleteEntryView(MethodView):
     def post(self, **kwargs):
         session = db.session()
         cu = flask_login.current_user
-        obj = models.JournalEntry.query.filter_by(id=kwargs['id']).first()
+        obj = models.JournalEntry.query.filter_by(id=kwargs["id"]).first()
         if not obj:
             flask.abort(404)
         if cu == obj.owner:
             old_date = obj.date_string
             session.delete(obj)
         session.commit()
-        return flask.redirect(flask.url_for('home', success=f'Deleted entry for {old_date}'))
+        return flask.redirect(
+            flask.url_for("home", success=f"Deleted entry for {old_date}")
+        )
 
 
 class LatestView(MethodView):
     def get(self, **kwargs):
         u = flask_login.current_user
         latest = u.get_latest_entry()
-        print('latest', latest)
-        print('user', u.query_all_entries().order_by(
-            models.JournalEntry.create_date.desc()).first())
+        print("latest", latest)
+        print(
+            "user",
+            u.query_all_entries()
+            .order_by(models.JournalEntry.create_date.desc())
+            .first(),
+        )
         if latest is not None:
             return flask.redirect(api.link_for_entry(latest))
         else:
@@ -382,20 +419,22 @@ class LatestView(MethodView):
 
 
 def add_views(app):
-    app.add_url_rule('/', view_func=IndexView.as_view('index'))
-    app.add_url_rule('/register', view_func=RegisterView.as_view('register'))
-    app.add_url_rule('/home', view_func=HomeView.as_view('home'))
-    app.add_url_rule('/latest', view_func=LatestView.as_view('latest_entry'))
-    app.add_url_rule('/export', view_func=ExportJournalView.as_view('export_journal'))
-    app.add_url_rule('/settings', view_func=SettingsView.as_view('settings'))
-    app.add_url_rule('/edit/new', view_func=EntryCreateView.as_view('create_entry'))
-    app.add_url_rule('/edit/<int:id>', view_func=EntryEditView.as_view('edit_entry'))
-    app.add_url_rule('/delete/<int:id>', view_func=DeleteEntryView.as_view('delete_entry'))
+    app.add_url_rule("/", view_func=IndexView.as_view("index"))
+    app.add_url_rule("/register", view_func=RegisterView.as_view("register"))
+    app.add_url_rule("/home", view_func=HomeView.as_view("home"))
+    app.add_url_rule("/latest", view_func=LatestView.as_view("latest_entry"))
+    app.add_url_rule("/export", view_func=ExportJournalView.as_view("export_journal"))
+    app.add_url_rule("/settings", view_func=SettingsView.as_view("settings"))
+    app.add_url_rule("/edit/new", view_func=EntryCreateView.as_view("create_entry"))
+    app.add_url_rule("/edit/<int:id>", view_func=EntryEditView.as_view("edit_entry"))
+    app.add_url_rule(
+        "/delete/<int:id>", view_func=DeleteEntryView.as_view("delete_entry")
+    )
 
     # generate endpoints for search view
-    search_view = EntrySearchView.as_view('entry')
-    delimiter = '-'
-    url_args = ['<int:year>', '<int:month>', '<int:day>']
+    search_view = EntrySearchView.as_view("entry")
+    delimiter = "-"
+    url_args = ["<int:year>", "<int:month>", "<int:day>"]
     # construct url endpoints for searching dates with increasing precision
-    endpoint = '/entry/' + delimiter.join(url_args)
+    endpoint = "/entry/" + delimiter.join(url_args)
     app.add_url_rule(endpoint, view_func=search_view)
